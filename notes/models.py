2ed92e3891d3a241f5afd5c917690f47fc6d296f
marcospaulorr <marcospauloricartereboucas@gmail.com>
2024-04-23 10:25:09 -0300

    inincio

---
+++ notes/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class StockData(models.Model):
+    date = models.DateField()
+    open = models.DecimalField(max_digits=10, decimal_places=2)
+    high = models.DecimalField(max_digits=10, decimal_places=2)
+    low = models.DecimalField(max_digits=10, decimal_places=2)
+    close = models.DecimalField(max_digits=10, decimal_places=2)
+    volume = models.BigIntegerField()
+
+    def __str__(self):
+        return f"{self.date} - Open: {self.open}, Close: {self.close}"
